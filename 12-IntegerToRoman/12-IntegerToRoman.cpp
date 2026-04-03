f3b9fae82aeaa9f6a768f3620dd7ead6f222eb43
Harshith404 <harshithshetty2006@gmail.com>
2026-04-03 19:39:23 +0530

    Create 12-IntegerToRoman

---
+++ 12-IntegerToRoman/12-IntegerToRoman.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/3/2026, 7:39:22 PM
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int, string>> val = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+            {1, "I"}
+        };
+
+        string result = "";
+
+        for (auto it : val) {
+            while (num >= it.first) {
+                result += it.second;
+                num -= it.first;
+            }
+        }
+
+        return result;
+    }
+};
