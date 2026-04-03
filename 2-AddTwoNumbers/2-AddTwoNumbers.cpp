d1660eab2ed8fe4870089efeccfd4e0f625c7b97
Harshith404 <harshithshetty2006@gmail.com>
2026-04-03 19:39:33 +0530

    Create 2-AddTwoNumbers

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -0,0 +1,29 @@
+// Last updated: 4/3/2026, 7:39:33 PM
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        int carry = 0;
+
+        while (l1 != NULL || l2 != NULL || carry) {
+            int sum = carry;
+
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            temp->next = new ListNode(sum % 10);
+            temp = temp->next;
+        }
+
+        return dummy->next;
+    }
+};//chatgpt
